bb63b4baa2334b7faddb4e1f1a6f51dbec3eff36
pollia <3152668032@qq.com>
2020-09-29 14:59:51 +0800

    重新clone

--- function/getsend.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-def getpicture():
-    url = "http://47.102.118.1:8089/api/problem?stuid=031802230"# 发送get请求
-    r = requests.get(url)# 获取返回的json数据
-    r = json.dumps(r.json())
-    r = json.loads(r)
-    return r
